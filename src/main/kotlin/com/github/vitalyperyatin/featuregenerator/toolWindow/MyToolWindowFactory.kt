a98951bf4aeb0cba42efc56d16e44a968c9fa788
GitHub Action <action@github.com>
2024-03-16 16:35:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/vitalyperyatin/featuregenerator/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/vitalyperyatin/featuregenerator/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.vitalyperyatin.featuregenerator.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.vitalyperyatin.featuregenerator.MyBundle
+import com.github.vitalyperyatin.featuregenerator.services.MyProjectService
 import javax.swing.JButton
 
 
